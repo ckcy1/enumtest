b36d5032c284d20817357959e69390ee9984c3f7
andy <ckcy1@qq.com>
2022-10-09 18:27:37 +0800

    一分

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,20 @@ enum Cion {
 }
 impl Cion{
     fn shucion(cion:Cion){
-match cion {
-    Cion::yifen =>{
-        println!("一分前")
-    }
-    Cion::wufen =>{
-        println!("五分前")
-    }
-    _ =>{
-        println!("无钱")
-    }
-}
+// match cion {
+//     Cion::yifen =>{
+//         println!("一分前")
+//     }
+//     Cion::wufen =>{
+//         println!("五分前")
+//     }
+//     _ =>{
+//         println!("无钱")
+//     }
+// }
+        if let cion = Cion::yifen {
+            println!("一分钱")
+        }
     }
 }
 
