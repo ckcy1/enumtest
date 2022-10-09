2e6f893181a0a9e1b207cb8dc17737a56f81fe33
andy <ckcy1@qq.com>
2022-10-09 19:25:54 +0800

    OPTION

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,38 @@
-
-
-enum Cion {
-    yifen,
-    wufen,
-    yijiao,
-    yiyuan,
-}
-impl Cion{
-    fn shucion(cion:Cion){
-// match cion {
-//     Cion::yifen =>{
-//         println!("一分前")
-//     }
-//     Cion::wufen =>{
-//         println!("五分前")
-//     }
-//     _ =>{
-//         println!("无钱")
+//
+//
+// enum Cion {
+//     yifen,
+//     wufen,
+//     yijiao,
+//     yiyuan,
+// }
+// impl Cion{
+//     fn shucion(cion:Cion){
+// // match cion {
+// //     Cion::yifen =>{
+// //         println!("一分前")
+// //     }
+// //     Cion::wufen =>{
+// //         println!("五分前")
+// //     }
+// //     _ =>{
+// //         println!("无钱")
+// //     }
+// // }
+//         if let cion = Cion::yifen {
+//             println!("一分钱")
+//         }
 //     }
 // }
-        if let cion = Cion::yifen {
-            println!("一分钱")
-        }
-    }
-}
+
 
 fn main() {
-    let yi = Cion::yifen;
-    Cion::shucion(yi);
-
+    // let yi = Cion::yifen;
+    // Cion::shucion(yi);
+    //
+    let x:u32=8;
+    let y:Option<u32>=None;
+    println!("{}",x+y.unwrap_or(0));
 
     println!("Hello, world!");
 }
